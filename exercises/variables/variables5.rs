91fe73b68da1021f28785d6ca74317685e34462e
司马凇 <1368470804@qq.com>
2021-01-26 18:38:54 +0800

    study the variable

--- exercises/variables/variables5.rs
+++ exercises/variables/variables5.rs
@@ -5,6 +5,7 @@
 
 fn main() {
     let number = "T-H-R-E-E";
+    let mut number=45;
     println!("Spell a Number : {}", number);
     number = 3;
     println!("Number plus two is : {}", number + 2);
