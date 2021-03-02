7a3b302d02dc3e279afe2b58af45ef5b3f427858
司马凇 <1368470804@qq.com>
2021-03-02 17:53:25 +0800

    complete the struct test and know a little panic

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -16,6 +16,7 @@ impl Package {
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
             // Something goes here...
+            panic!("error");    // use panic! macro
         } else {
             return Package {
                 sender_country,
@@ -25,12 +26,19 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
+    fn is_international(&self)-> bool {
         // Something goes here...
+        if self.weight_in_grams > 0 {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
         // Something goes here... 
+        return self.weight_in_grams*cents_per_gram;
     }
 }
 
@@ -62,7 +70,7 @@ mod tests {
         let sender_country = String::from("Spain");
         let recipient_country = String::from("Spain");
 
-        let cents_per_gram = ???;
+        let cents_per_gram = 3;
 
         let package = Package::new(sender_country, recipient_country, 1500);
 
